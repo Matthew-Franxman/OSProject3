17c320d9ed8d8cc85a220b33a03b60c19858897b
Tom <tommycheal@hotmail.com>
2020-11-02 13:20:40 -0500

    Created Semaphores and initialized shared memory

--- server.c
+++ server.c
@@ -1,5 +1,67 @@
-int main( int argc, char *argv[]){
+#include <stdio.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-    
-    return 0;
+#define MAX_DIR_PATH 1024
+#define MAX_KEYWORD 256
+#define MAX_LINE_SIZE 1024
+#define MAX_OUT_SIZE 2048
+#define SIZE_OF_QUEUE 8
+
+#define SEM_MUTEX "/sem_mutex"
+#define SEM_COUNT "/sem_count"
+
+#define MEMORY_NAME "/queue_of_keywords"
+
+struct readyQueue{
+    char *keywords[MAX_KEYWORD];
+};
+
+int main( int argc, char *argv[]){
+    if(argc != 3){
+        printf("Invalid Command Line Arguments");
+        return 0;
+    }
+
+    int requiredQueueSize = *argv[1];
+    int bufferSize = *argv[2];
+
+    struct readyQueue *queue;
+    sem_t *mutex, *count;
+    int sharedMemory;
+
+    //exclusion semaphore
+    if((mutex = sem_open(SEM_MUTEX, O_CREAT , 0666, 0)) == SEM_FAILED){
+            printf("Error opening mutex");
+            return 0;
+    }
+
+    //open shared memory
+    if((sharedMemory = shm_open(MEMORY_NAME, O_RDWR | O_CREAT |O_EXCL, 0660) == -1)){
+        printf("Error opening shared memory");
+        return 0;
+    }
+
+    //set shared memory size
+    if( ftruncate(sharedMemory, sizeof(struct readyQueue)) == -1){
+        printf("Error truncating memory");
+        return 0;
+    }
+
+    //map the memory
+    if((queue = mmap(NULL, sizeof(struct readyQueue), PROT_READ | PROT_WRITE, MAP_SHARED, sharedMemory, 0 )) == MAP_FAILED){
+        printf("Error mapping memory");
+        return 0;
+    }
+
+    //counting semaphore used to indicate how many spots are available in the queue
+    if((count = sem_open(SEM_COUNT, O_CREAT | O_EXCL, 0660, requiredQueueSize)) == SEM_FAILED){
+        printf("Error opening count");
+        return 0;
+    }
+
+
 }
